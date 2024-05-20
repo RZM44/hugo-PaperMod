b690f4006c6a6b20a21a7a8f7b01c449ca30459a
RZM44 <zqz235711@gmail.com>
2024-05-20 11:55:58 +0800

    自定修改主题

--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -88,7 +88,7 @@ sInput.onkeyup = function (e) {
             let resultSet = ''; // our results bucket
 
             for (let item in results) {
-                resultSet += `<li class="post-entry"><header class="entry-header">${results[item].item.title}&nbsp;»</header>` +
+                resultSet += `<li class="post-entry"><header class="entry-header"><h2 class="entry-hint-parent">${results[item].item.title}&nbsp;»</h2></header>` +
                     `<a href="${results[item].item.permalink}" aria-label="${results[item].item.title}"></a></li>`
             }
 
